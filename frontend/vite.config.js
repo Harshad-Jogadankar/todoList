ac3e135876d36cc490cad51f3854401ca7ae1c19
Harshad-Jogadankar <107821691+Harshad-Jogadankar@users.noreply.github.com>
2024-06-20 12:30:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+  host: "0.0.0.0",
+  port: 5173,
+  },
 });
 // export default defineConfig({
 //   plugins: [react()],
-//   server: {
-//     host: "0.0.0.0",
-//     port: 5173,
-//   },
+//   
 // });
 
 // export default defineConfig({
